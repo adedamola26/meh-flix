84024af6347417261aff920df49ebeb5c0bb0f62
adedamola26 <adesoye.a@northeastern.edu>
2024-04-27 18:36:39 -0400

    resize images and set page configurations

--- pages/3_Collaborative-filtering.py
+++ pages/3_Collaborative-filtering.py
@@ -1,13 +1,11 @@
 import streamlit as st
-from project_utils.utils import get_movie_data, truncate_title, movie_df, get_neighbours
+from project_utils.utils import get_movie_data, truncate_title, movie_df, get_neighbours, get_and_resize_image
 
 
 st.set_page_config(
-    page_title="Find Songs Similar to Yours🎤", page_icon="🎤", layout="wide"
+    page_title="Collaborative-filtering", page_icon="👥", layout="wide"
 )
 
-
-    
 movie_choice = st.selectbox(
     'Your favorite movie:',
     movie_df.title,
@@ -17,15 +15,17 @@ movie_choice = st.selectbox(
 
 if st.button('Recommend'):
     recommendations = get_neighbours(movie_choice)
-    names, posters = get_movie_data(recommendations)
+    names, images_url = get_movie_data(recommendations)
 
     col1, col2 = st.columns(2)
     for i in range(5):
         with col1:
             st.subheader(truncate_title(names[2*i]))
-            st.image(posters[2*i])
+            image = get_and_resize_image(images_url[2*i], 750)
+            st.image(image)
             
         with col2:
             st.subheader(truncate_title(names[2*i+1]))
-            st.image(posters[2*i+1])
+            image = get_and_resize_image(images_url[2*i+1], 750)
+            st.image(image)
             
